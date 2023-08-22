4b7ae43a1ffe2a7f15bd97fb5e178cc795beb27b
dineth Dilshan <E2145112@bit.uom.lk>
2023-08-22 22:43:35 +0530

    New Application build
    Using emposising and emposized and use state Start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const changeName = (event) => {
-    console.log(event.target.value);
-    setName(event.target.value);
-    // const Names = <h2>{event.target.value} </h2>;
-  };
-
-  const [name, setName] = useState("Dineth");
-  return (
-    <div className="App">
-      <h1> Hellow </h1>{" "}
-      <div>{name && name ? <h1>Hi! {name}</h1> : <p>Enter Name</p>}</div>
-      <br />
-      <input type="text" placeholder="Enter Text" onChange={changeName} />
-    </div>
-  );
+  return <div className="App">Hellow World</div>;
 }
 
 export default App;
