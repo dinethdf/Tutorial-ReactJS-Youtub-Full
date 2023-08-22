ef48aa5fd401405b0ec68ee9e9ed73b45da501dc
dineth Dilshan <E2145112@bit.uom.lk>
2023-08-22 15:25:57 +0530

    Button and text box
    `Button and text box work and hooks and other things use

--- src/App.js
+++ src/App.js
@@ -5,16 +5,25 @@ function App() {
   const changeName = (event) => {
     console.log(event.target.value);
     setName(event.target.value);
-    // const Names = <h2>{event.target.value} </h2>;
+  };
+
+  const hideShow = () => {
+    showtest === true ? setShowtest(false) : setShowtest(true);
   };
 
   const [name, setName] = useState("");
+  const [showtest, setShowtest] = useState(true);
   return (
     <div className="App">
-      <h1> Hellow </h1>{" "}
-      <div>{name && name ? <h1>Hi! {name}</h1> : <p>Enter Name</p>}</div>
+      <h1> Hellow </h1>
+      {showtest === true ? (
+        <div>{name && name ? <h1>Hi! {name}</h1> : <p>Enter Name</p>}</div>
+      ) : (
+        "Welcome"
+      )}
       <br />
       <input type="text" placeholder="Enter Text" onChange={changeName} />
+      <button onClick={hideShow}>Click Me</button>
     </div>
   );
 }
