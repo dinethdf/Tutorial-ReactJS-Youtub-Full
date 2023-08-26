3e257b352853d2231710620e734fbe6f74806e66
dineth Dilshan <E2145112@bit.uom.lk>
2023-08-26 07:48:36 +0530

    css adding

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 // import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hellow World</div>;
+  const [todoList, setTodoList] = useState([]);
+  const [newtask, setNewtask] = useState();
+
+  const gettext = (event) => {
+    setNewtask(event.target.value);
+  };
+  const addTodolist = () => {
+    setTodoList([...todoList, newtask]);
+  };
+  return (
+    <div className="App" style={{ backgroundColor: "gray", padding: "80px" }}>
+      <input type="text" onChange={gettext} />
+      <button onClick={addTodolist}>Add Task</button>
+      <div className="list">
+        {todoList.map((task) => {
+          return (
+            <div className="tasklist">
+              <h1>{task}</h1>
+              <button>X</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
